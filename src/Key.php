7d9ceeb9597fe457ef34fed969c9723c0cd3d253
Scott <scott@paragonie.com>
2015-07-22 12:41:58 -0400

    Close #1

--- src/Key.php
+++ src/Key.php
@@ -4,10 +4,14 @@ namespace ParagonIE\Halite;
 final class Key
 {
     private $secretbox_key;
+    const MIN_COMPRESSED_KEYSIZE = 24; // 75% of CRYPTO_SECRETBOX_KEYBYTES
     
     public function __construct($key = null)
     {
-        $this->secretbox_key = $key;
+        if ($key !== null) {
+            $this->testKeyEntropy($key);
+            $this->secretbox_key = $key;
+        }
     }
     
     /**
@@ -17,9 +21,11 @@ final class Key
      */
     public function generate()
     {
-        $this->secretbox_key = \Sodium::randombytes_buf(
-            \Sodium::CRYPTO_SECRETBOX_KEYBYTES
-        );
+        do {
+            $this->secretbox_key = \Sodium::randombytes_buf(
+                \Sodium::CRYPTO_SECRETBOX_KEYBYTES
+            );
+        } while ($this->testKeyEntropy($key, true));
         return $this;
     }
     
@@ -75,5 +81,30 @@ final class Key
             throw new \Exception('No encryption key was set!');
         }
         return $this->secretbox_key;
+    }
+    
+    /**
+     * Test that a given key is the proper length and has sufficient entropy
+     * 
+     * @param string $key Should be a 32-byte random key
+     * @return boolean
+     * @throws \Exception
+     */
+    private function testKeyEntropy($key, $dont_throw = false)
+    {
+        if (mb_strlen($key, '8bit') !== \Sodium::CRYPTO_SECRETBOX_KEYBYTES) {
+            if ($dont_throw) {
+                return false;
+            }
+            throw new \Exception("You must use an encryption key. A password will not work. Use derive() instead.");
+        }
+        $compressed = \gzcompress($key, 9);
+        if (mb_strlen($key, '8bit') < self::MIN_COMPRESSED_KEYSIZE) {
+            if ($dont_throw) {
+                return false;
+            }
+            throw new \Exception("You must use an encryption key. A password will not work. Use derive() instead.");
+        }
+        return true;
     }
 }
