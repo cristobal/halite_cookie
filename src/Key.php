9cc7b3ae26b9fa03999dc8ab83d8a18074077595
Scott <scott@paragonie.com>
2015-07-26 01:14:30 -0400

    Account for gzip overhead, count bits

--- src/Key.php
+++ src/Key.php
@@ -4,8 +4,10 @@ namespace ParagonIE\Halite;
 final class Key
 {
     private $secretbox_key;
-    const MIN_COMPRESSED_KEYSIZE = 24; // 75% of CRYPTO_SECRETBOX_KEYBYTES
-    const MIN_COMPRESSED_SALTSIZE = 24; // 75% of CRYPTO_PWHASH_SCRYPTSALSA208SHA256_SALTBYTES
+    const MIN_COMPRESSED_KEYSIZE = 32; // 75% of CRYPTO_SECRETBOX_KEYBYTES + 8 overhead
+    const MIN_COMPRESSED_SALTSIZE = 32; // 75% of CRYPTO_PWHASH_SCRYPTSALSA208SHA256_SALTBYTES + 8 overhead
+    const BITMIN = 112;
+    const BITMAX = 144;
     
     public function __construct($key = null)
     {
@@ -81,6 +83,30 @@ final class Key
     }
     
     /**
+     * Count the number of bits in a string
+     * 
+     * @param string $str
+     * @return array
+     */
+    private function countBits($str)
+    {
+        $bits = [0,0];
+        $len = mb_strlen($str, '8bit');
+        for ($i = 0; $i < $len; ++$i) {
+            $c = ord($str[$i]);
+            for ($j = 0; $j < 8; ++$j) {
+                if (($c & 0x01) === 0) {
+                    ++$bits[0];
+                } else {
+                    ++$bits[1];
+                }
+                $c >>= 1;
+            }
+        }
+        return $bits;
+    }
+    
+    /**
      * Test that a given key is the proper length and has sufficient entropy
      * 
      * @param string $key Should be a 32-byte random key
@@ -96,14 +122,21 @@ final class Key
             }
             throw new \Exception("You must use an encryption key. A password will not work. Use generate() to create a proper encryption key.");
         }
-        $compressed = \gzcompress($key, 9);
-        if (mb_strlen($compressed, '8bit') < self::MIN_COMPRESSED_KEYSIZE) {
+        $compressed = \gzcompress($key, 1);
+        if (\mb_strlen($compressed, '8bit') < self::MIN_COMPRESSED_KEYSIZE) {
+            if ($dont_throw) {
+                return false;
+            }
+            throw new \Exception("You must use a random salt. A password will not work.");
+        }
+        
+        $bits = $this->countBits($key);
+        if ($bits[0] < self::BITMIN || $bits[1] < self::BITMIN || $bits[0] > self::BITMAX || $bits[1] > self::BITMAX) {
             if ($dont_throw) {
                 return false;
             }
             throw new \Exception("You must use an encryption key. A password will not work. Use generate() to create a proper encryption key.");
         }
-        return true;
     }
     
     /**
@@ -122,13 +155,21 @@ final class Key
             }
             throw new \Exception("You must use a random salt. A password will not work.");
         }
-        $compressed = \gzcompress($salt, 9);
+        $compressed = \gzcompress($salt, 1);
         if (mb_strlen($compressed, '8bit') < self::MIN_COMPRESSED_SALTSIZE) {
             if ($dont_throw) {
                 return false;
             }
             throw new \Exception("You must use a random salt. A password will not work.");
         }
-        return true;
+        
+        $bits = $this->countBits($salt);
+        
+        if ($bits[0] < self::BITMIN || $bits[1] < self::BITMIN || $bits[0] > self::BITMAX || $bits[1] > self::BITMAX) {
+            if ($dont_throw) {
+                return false;
+            }
+            throw new \Exception("You must use a random salt. A password will not work.");
+        }
     }
 }
